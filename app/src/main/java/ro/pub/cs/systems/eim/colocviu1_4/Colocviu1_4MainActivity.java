199163ec66b892c693ec77ccf6164a552d741537
andrei <andrei.orasteanu@stud.acs.upb.ro>
2021-04-15 06:53:59 +0300

    first commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_4/Colocviu1_4MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.colocviu1_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colocviu1_4MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colocviu1_4_main);
+    }
+}
